0320890969497adb9fa544fec2ea2a353241c2b1
Andrew Ewen <andy1633@hotmail.co.uk>
2016-12-11 18:53:11 +0000

    Complete day 9.

--- day_9_cyberspace_explosives.py
+++ day_9_cyberspace_explosives.py
@@ -1,15 +1,30 @@
 #!/usr/bin/env python3
 import re
 
-filename = 'input_files/9.txt'
-with open(filename) as inputfile:
-    inputstring = inputfile.read()
-
-    
-def replace(m):
-    num_characters = int(m.groups()[0])
-    repeat_count = int(m.groups()[1])
-    return m.string[m.end():m.end() + num_characters] * repeat_count
-
 marker_pattern = r'\((\d+)x(\d+)\)'
-print(len(re.sub(marker_pattern, replace, inputstring)))
+
+def decompress(s):
+    m = re.search(marker_pattern, s)
+    if m:
+        num_chars = int(m.group(1))
+        num_repeat = int(m.group(2))
+        start = m.start()
+        end = m.end()
+        return (start + 
+                decompress(s[end:end + num_chars] * num_repeat) +
+                decompress(s[end + num_chars:]))
+    else:
+        return len(s)
+
+def main():
+    filename = 'input_files/9.txt'
+    with open(filename) as inputfile:
+        inputstring = inputfile.read()
+        
+    decompressed = decompress(re.sub(r'\s', '', inputstring))
+    print(decompressed)
+    
+if __name__ == '__main__':
+    main()
+
+
