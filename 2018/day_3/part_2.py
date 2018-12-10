8369dd404adf5330aeb8f6cb09b5fc41f4306055
Andrew Ewen <andy1633@hotmail.co.uk>
2018-12-10 16:00:00 +0000

    Complete day 3.

--- 2018/day_3/part_2.py
+++ 2018/day_3/part_2.py
@@ -5,11 +5,13 @@ from collections import defaultdict
 square_re = r'#(?P<id>\d+)\s+@\s+(?P<x>\d+),(?P<y>\d+):\s+(?P<w>\d+)x(?P<h>\d+)'
 square_re = re.compile(square_re)
 
+
 def process_line(l):
     square = square_re.match(l).groupdict()
     for k in square:
         square[k] = int(square[k])
     return square
+
 
 with open('input.txt') as fp:
     squares = [process_line(line) for line in fp]
@@ -20,17 +22,18 @@ for square in squares:
         for i in range(square['x'], square['x'] + square['w']):
             rectangle[(i, j)].append(square['id'])
 
-overlapping = defaultdict(set)
-for square_inch in rectangle:
-    for a in square_inch:
-        for b in square_inch:
-            overlapping[a].add(b)
+for square in squares:
+    is_overlapping = False
+    for j in range(square['y'], square['y'] + square['h']):
+        for i in range(square['x'], square['x'] + square['w']):
+            if len(rectangle[(i, j)]) > 1:
+                is_overlapping = True
+                break
+        if is_overlapping:
+            break
 
-print(min(overlapping.values(), key=lambda s: len(s)))
-
-for square_id, overlapping in overlapping.items():
-    if len(overlapping) == 1:
-        print(f'Answer is: {square_id}')
+    if not is_overlapping:
+        print(f'Square #{square["id"]} does not overlap')
         break
 else:
-    print('Could not find a non overlapping square.')
+    print('Could not find a non-overlapping square.')
