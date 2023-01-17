6a2c08687a8eb1b2e3b82439ff092b3a3b438f12
Andrew Ewen <andy1633@hotmail.co.uk>
2023-01-17 16:15:25 +0000

    Complete day 6.

---
+++ 2022/day_06/part_1.rs
@@ -0,0 +1,20 @@
+use std::collections::HashSet;
+use std::iter::FromIterator;
+use std::fs;
+
+const CHUNK_SIZE: usize = 4;
+
+fn main() {
+    let file_contents = fs::read_to_string("input.txt").unwrap();
+
+    let len = file_contents.len();
+    for i in 0..len - CHUNK_SIZE {
+        let chunk = &file_contents[i..i+CHUNK_SIZE];
+        let chunk_set: HashSet<char> = HashSet::from_iter(chunk.chars());
+
+        if chunk_set.len() == CHUNK_SIZE {
+            println!("{}", i + CHUNK_SIZE);
+            break;
+        }
+    }
+}
